af5dc01443fbed089b119c6925b7f26992ea6a64
MoonSeokJun <tjrwns2513@naver.com>
2024-05-30 15:38:35 +0900

    20240530_~16

---
+++ src/Java0530_6.java
@@ -0,0 +1,27 @@
+import java.security.spec.RSAOtherPrimeInfo;
+import java.util.Arrays;
+import java.util.List;
+
+public class Java0530_6 {
+
+  public static void main(String[] args) {
+//    int[] arr = {1, 2, 3, 4, 5};
+//    int[] arr2 = new int[5];
+
+//    List<Integer> list = Arrays.asList(1);
+//    System.out.println(list.get(0));
+
+    int a = 10; // 기본 자료형
+//    int[] arr = {1,2,3,4,5};
+//    System.out.println(arr[3]);
+//    int[] arr = new int[5]; // 참조 자료형
+
+//    String[] arr = {"o", "k", "y"} // 이렇게도 가능
+//    String[] arr = new String[3];
+
+//    String name = "sung yeon";
+//    String name2 = new String("sung yeon");
+//    System.out.println(name);
+//    System.out.println(name2);
+  }
+}
