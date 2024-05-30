c7c69b4900cfaac7803ba49279cd6a4de878ec17
MoonSeokJun <tjrwns2513@naver.com>
2024-05-30 13:07:04 +0900

    20240530_~12

--- src/Test.java
+++ src/Test.java
@@ -5,11 +5,20 @@ public class Test {
  * @deprecated asdasd
   **/
     public static void main(String[] args) {
+        // {타입} 변수명 = 값;
+
+        // 정수 = int
         int a = 10;
-        System.out.println(a);
-        System.out.println(a);
-        System.out.println(a);
-        System.out.println(a);
-        System.out.println(a);
+
+        // 실수 = double
+        double b = 0.01;
+
+        // 미리 선언 해 준 후 값을 넣어줄 수 있다.
+        int q,w,e,r;
+        q = 10;
+        w = 20;
+        e = 30;
+        r = 40;
+
     }
 }
---
+++ src/java0530_3.java
@@ -0,0 +1,12 @@
+public class java0530_3 {
+
+  public static void main(String[] args) {
+
+    // 변수 3개 이용해서 합 100 만들기
+    int a = 30;
+    int b = 40;
+    int c = 30;
+    System.out.println(a + b + c);
+    int q = 20, w = 20, e = 20;
+  }
+}
