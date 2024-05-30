4c83cda8135e8bd38ff3ddfa218cd6b35525e37e
MoonSeokJun <tjrwns2513@naver.com>
2024-05-30 15:38:29 +0900

    20240530_~15

---
+++ src/Java0530_5.java
@@ -0,0 +1,31 @@
+public class Java0530_5 implements TestInterface {
+
+  public static void main(String[] args) {
+    //    int a = 10 + 2;
+    //    System.out.println(a);
+
+    //    // float 타입 변수 선언과 초기화
+    //    float myFloat = 9.75f; // 숫자 뒤에 'f'를 붙임
+    //
+    //    // float를 사용한 계산
+    //    float anotherFloat = myFloat * 2;
+    //
+    //    // 결과 출력
+    //    System.out.println("Original float value: " + myFloat);
+    //    System.out.println("Calculated float value: " + anotherFloat);
+
+    boolean isTrue = false;
+
+    if (!isTrue) {
+      System.out.println("이건 트루입니다.");
+    } else {
+      System.out.println("이건 펄스입니다.");
+    }
+
+  }
+
+  @Override
+  public void test() {
+
+  }
+}
