a4e5ad36077bffe0233788c06cc6065c2b17582a
MoonSeokJun <tjrwns2513@naver.com>
2024-05-31 13:28:43 +0900

    0531_~10

---
+++ src/Java0531/Java0531_1.java
@@ -0,0 +1,28 @@
+package Java0531;
+
+public class Java0531_1 {
+  public static void main(String[] args) {
+
+    //    String firstName = "seok jun";
+    //    String secondName = "moon";
+    //    String phone = "galaxy";
+    //    String status = "happy";
+    //    System.out.println("나는 " + secondName + " " + firstName + "입니다");
+    //    System.out.println(phone + " is " + status);
+    //
+    //    System.out.println(firstName.length());
+
+    // 주소값이 같은지
+    //    String a = "hello";
+    //    String b = new String("hello");
+    //
+    //    System.out.println(a.equals(b)); // true
+    //    System.out.println(a == b); // false
+
+//    // 문자 찾기 indexOf
+//    String a = "Hello World!";
+//    System.out.println(a.indexOf("!"));
+
+
+  }
+}
