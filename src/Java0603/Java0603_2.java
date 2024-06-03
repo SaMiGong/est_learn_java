872de068ab76dbec4a2194b30aeaddda51ddd8e2
MoonSeokJun <tjrwns2513@naver.com>
2024-06-03 10:51:10 +0900

    0603_~11

---
+++ src/Java0603/Java0603_2.java
@@ -0,0 +1,35 @@
+package Java0603;
+
+import javax.swing.*;
+
+public class Java0603_2 {
+  public static void main(String[] args) {
+    //    // AND (&&)
+    //    System.out.println(true && true); // true
+    //    System.out.println(true && false); // false
+    //    System.out.println(false && true); // false
+    //    System.out.println(false && false); // false
+    //
+    //    // OR (||)
+    //    System.out.println(true || true); // true
+    //    System.out.println(true || false); // true
+    //    System.out.println(false || true); // true
+    //    System.out.println(false || false); // false
+    //
+    //    // XOR (베타적 논리합)
+    //    System.out.println(true ^ true); // false
+    //    System.out.println(true ^ false); // true
+    //    System.out.println(false ^ true); // true
+    //    System.out.println(false ^ false); // false
+    //
+    //    // NOT (논리 부정)
+    //    System.out.println(!true); // false
+    //    System.out.println(!false); // true
+
+    //    // i는 2의 배수이면서 3의 배수이다
+    //    i % 2 == 0 && i % 3 == 0
+    //    // x는 30의 배수이면서 2의 배수이거나 5의 배수이다.
+    //    (x % 30 == 0) && ((x % 2 ==0 ) || (x%5==0))
+
+  }
+}
