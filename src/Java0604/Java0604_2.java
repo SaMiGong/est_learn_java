43b84a70aa331315c9d63a5d0659826f3ad2a96d
MoonSeokJun <tjrwns2513@naver.com>
2024-06-04 12:51:43 +0900

    0604_~11

---
+++ src/Java0604/Java0604_2.java
@@ -0,0 +1,59 @@
+package Java0604;
+
+public class Java0604_2 {
+  public static void main(String[] args) {
+
+    //    int i = 0;
+    //    while (i <= 300) {
+    //      if (i % 3 == 0) {
+    //        System.out.println(i);
+    //      }
+    //      i++;
+    //    }
+
+    //        int[] numbers = {10, 5, 8, 20, 3, 15, 9, 2};
+    //        int i = 0;
+    //        int max = numbers[0];
+    //        while (i < numbers.length) {
+    //          if (max < numbers[i]) {
+    //            max = numbers[i];
+    //          }
+    //          i++;
+    //        }
+    //        System.out.println(max);
+
+    //    int[] numbers = {10, 5, 8, 20, 3, 15, 9, 2};
+    //
+    //    int max = numbers[0];
+    //    int i = 1;
+    //    while(i<numbers.length){
+    //      if(numbers[i] > max){
+    //        max = numbers[i];
+    //      }
+    //      i++;
+    //    }
+    //    System.out.println(max);
+
+    //    int p = 0;
+    //    int m = 0;
+    //    int i = 0;
+    //    int[] numbers = {4, -2, 9, -7, 5, 1, -3, 6, -1, 8};
+    //    while (i < numbers.length) {
+    //      if (numbers[i] > 0) {
+    //        p += numbers[i];
+    //      } else {
+    //        m += numbers[i];
+    //      }
+    //      i++;
+    //    }
+    //    System.out.println("양수: " + p);
+    //    System.out.println("음수: " + m);
+
+    // 같은 결과값
+
+    //    int posNum = IntStream.of(numbers).filter(num -> num > 0).sum();
+    //    int neNum = IntStream.of(numbers).filter(num -> num < 0).sum();
+
+
+  }
+}
