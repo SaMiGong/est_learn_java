f89f74c7a94f517c7aab881d47997002da20e632
MoonSeokJun <tjrwns2513@naver.com>
2024-06-04 15:46:44 +0900

    0604_~14

---
+++ src/Java0604/Java0604_4.java
@@ -0,0 +1,33 @@
+package Java0604;
+
+public class Java0604_4 {
+  public static void main(String[] args) {
+    //    int[] arr = new int[6];
+    //    arr[0] = 1;
+    //    arr[1] = 1;
+    //    for (int n = 2; n < arr.length; n++) {
+    //      arr[n] = arr[n - 2] + arr[n - 1];
+    //    }
+    //    for (int i : arr) {
+    //      System.out.println(i);
+    //    }
+
+    //    for (int i = 0; i >= -100; i--) {
+    //      if (i % 2 == 0) {
+    //        System.out.println("짝수: " + i);
+    //      }
+    //    }
+
+//    int[] arr = new int[100];
+//    arr[0] = 1;
+//    arr[1] = 1;
+//    for (int n = 2; n < arr.length; n++) {
+//      arr[n] = arr[n - 2] + arr[n - 1];
+//      System.out.println(arr[n]);
+//    }
+
+    for(int i = 0; i < 10; i++) {
+
+    }
+  }
+}
