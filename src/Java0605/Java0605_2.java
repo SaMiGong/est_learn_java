b022deaeabccbd61d2532db1eeed2a413c278480
MoonSeokJun <tjrwns2513@naver.com>
2024-06-05 14:33:09 +0900

    240605_~11

---
+++ src/Java0605/Java0605_2.java
@@ -0,0 +1,16 @@
+package Java0605;
+
+import java.util.Scanner;
+
+public class Java0605_2 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+    System.out.println("정수를 입력해 주세요.");
+    int a = sc.nextInt();
+
+    System.out.println("제가 입력한 값은 " + a + " 입니다.");
+
+    sc.close();
+  }
+}
