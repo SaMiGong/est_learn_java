30eeb3dd64c485c4047f4b2221fcb057da5412d5
MoonSeokJun <tjrwns2513@naver.com>
2024-06-07 17:31:05 +0900

    240607_CT

---
+++ src/CodingTest0607/CodeTest06.java
@@ -0,0 +1,17 @@
+package CodingTest0607;
+
+public class CodeTest06 {
+    public static void main(String[] args) {
+
+        printHello(100);
+    }
+
+    static int printHello(int num) {
+        if (num == 0) {
+            return 0;
+        } else {
+            System.out.println(num);
+            return printHello(num - 1);
+        }
+    }
+}
