fafca49a5c5e3ba4c50bac557ec137800fff4c22
MoonSeokJun <tjrwns2513@naver.com>
2024-06-10 17:10:07 +0900

    240610 ~14

---
+++ src/CodingTest0610/CT05.java
@@ -0,0 +1,28 @@
+package CodingTest0610;
+
+public class CT05 {
+  public static void main(String[] args) {
+    /*
+    어떤 숫자가 11의 배수이거나 11의 배수보다 1 큰 숫자라면, 그 숫자를 "cool"하다고 하겠습니다.
+    주어진 0 이상의 숫자가 cool하다면 true를 반환하세요.
+
+    System.out.println(isCool(22)); // true (11의 배수)
+    System.out.println(isCool(23)); // true (11의 배수보다 1 큼)
+    System.out.println(isCool(24)); // false (11의 배수도 아니고, 11의 배수보다 1 크지도 않음)
+    */
+    System.out.println(isCool(23));
+  }
+
+  static boolean isCool(int a) {
+
+    // 방법 1
+    //    return a % 11 == 0 || a % 11 == 1;
+
+    // 방법 2
+    if (a >= 0) {
+
+      return a % 11 == 0 || a % 11 == 1;
+    }
+    return false;
+  }
+}
