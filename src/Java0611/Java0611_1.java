0e4e736cce2993d035cab0fe6e45d97e23f477e3
MoonSeokJun <tjrwns2513@naver.com>
2024-06-11 10:16:19 +0900

    240611 ~10:00

---
+++ src/Java0611/Java0611_1.java
@@ -0,0 +1,33 @@
+package Java0611;
+
+public class Java0611_1 {
+  public static void main(String[] args) {
+//    int okay = okay(1, 2);
+  }
+
+  //  static int okay(int a, int b) {
+  //
+  //    System.out.println(a + b);
+  //    return 1; // 리턴 만나면 함수는 끝나게된다.
+  //  }
+
+  static int add(int a, int b) {
+    return a + b;
+  }
+
+  static int sub(int a, int b) {
+    return a - b;
+  }
+
+  static int multiply(int a, int b) {
+    return a * b;
+  }
+
+  static int divide(int a, int b) {
+    return a / b;
+  }
+
+  static void login(String email, String password){
+
+  }
+}
