bb7c765b7ccd1360312541f375d39035727a9a88
MoonSeokJun <tjrwns2513@naver.com>
2024-06-13 15:47:59 +0900

    240613_~16:00

--- src/Java0613/Java1613_5/Animal.java
+++ src/Java0613/Java1613_5/Animal.java
@@ -1,3 +1,11 @@
 package Java0613.Java1613_5;
 
-public class Animal {}
+public abstract class Animal {
+  protected String kind;
+
+  public void breathe() {
+    System.out.println("숨을 쉽니다.");
+  }
+
+  public abstract void sound();
+}
---
+++ src/Java0613/Java1613_5/Dog.java
@@ -0,0 +1,12 @@
+package Java0613.Java1613_5;
+
+public class Dog extends Animal{
+
+    public Dog(){
+        this.kind="포유류";
+    }
+    @Override
+    public void sound() {
+    System.out.println("month month");
+    }
+}
