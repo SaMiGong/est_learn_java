72621cce5b0838cbaa8fc947b062190efc24a3e1
MoonSeokJun <tjrwns2513@naver.com>
2024-06-14 11:04:40 +0900

    240614_~11:00

--- src/Java0614/Test.java
+++ src/Java0614/Test.java
@@ -3,11 +3,14 @@ package Java0614;
 import java.util.Scanner;
 
 public class Test {
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-
-        scanner.nextInt();
-
-        scanner.close();
+  public static void main(String[] args) {
+    try {
+      int result = 10 / 2;
+      System.out.println(result);
+    } catch (ArithmeticException e) { // 예외가 터지지 않아서 실행안됨
+        // 언체크드 예외
+      System.out.println("0으로 나눌 수 없습니다.");
     }
+    System.out.println("정상적으로 종료되었습니다.");
+  }
 }
