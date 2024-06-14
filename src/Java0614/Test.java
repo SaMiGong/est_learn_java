198ca1e62bea91c11e4b646b75cdabb0c213a5a4
MoonSeokJun <tjrwns2513@naver.com>
2024-06-14 10:05:08 +0900

    240614_~10:00

---
+++ src/Java0614/Test.java
@@ -0,0 +1,13 @@
+package Java0614;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.nextInt();
+
+        scanner.close();
+    }
+}
